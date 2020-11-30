075bad7a7dcec60ad634e774f95359f128f800d5
RyanMKrol <ryankrol.m@gmail.com>
2020-11-30 17:34:34 +0000

    Add types file to manage jsdoc types

---
+++ src/modules/doc/types.js
@@ -0,0 +1,19 @@
+/* @module types */
+
+/**
+ * @typedef Request
+ * @type {object}
+ * @see https://www.npmjs.com/package/express
+ */
+
+/**
+ * @typedef Response
+ * @type {object}
+ * @see https://www.npmjs.com/package/express
+ */
+
+/**
+ * @typedef HttpError
+ * @type {object}
+ * @see https://www.npmjs.com/package/http-errors
+ */
