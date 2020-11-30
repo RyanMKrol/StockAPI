590f65f3f3c92eb2e6ece8a802a55f306e1ec9d0
RyanMKrol <ryankrol.m@gmail.com>
2020-11-30 17:06:04 +0000

    Add the tickers route
    * Can now fetch tickers from the API per index

---
+++ src/modules/routes/tickers.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+import { INDEXES_CONFIG, API_STORAGE_KEYS } from '../constants';
+import SERVER_DATA from '../data_structures';
+
+const router = express.Router();
+
+router.get('/:index', async (req, res) => {
+  const { index } = req.params;
+
+  if (!validateRequest(req)) {
+    res.status(400);
+    res.send({
+      error: 'Invalid Request',
+    });
+  } else {
+    const tickerData = SERVER_DATA.getData(API_STORAGE_KEYS.TICKERS) || {};
+    const tickers = tickerData[index];
+    if (tickers) {
+      res.send({ count: tickers.length, tickers });
+    } else {
+      res.status(500);
+      res.send({
+        error: 'No tickers',
+      });
+    }
+  }
+});
+
+/**
+ * Method to validate the request to the tickers API
+ *
+ * @param {any} request A request of some kidn
+ */
+function validateRequest(request) {
+  const { index } = request.params;
+  const supportedTickerIndexes = INDEXES_CONFIG.getSupportedTickersIndexes();
+
+  return supportedTickerIndexes.includes(index);
+}
+
+export default router;
