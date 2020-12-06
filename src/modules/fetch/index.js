4f1750682578186b38578c2fdaba10a9d6931e10
RyanMKrol <ryankrol.m@gmail.com>
2020-12-06 17:52:25 +0000

    Add fetch module for fetching stock prices

--- src/modules/fetch/index.js
+++ src/modules/fetch/index.js
@@ -1,3 +1,4 @@
 export { default as fetchFundamentalsData } from './fundamentals';
 export { default as fetchTickers } from './tickers';
 export { default as fetchHeatmap } from './heatmaps';
+export { default as fetchPrices } from './prices';
