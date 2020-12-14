7c18cfe382f7bd1cbfccd5c19c590a19e6e72377
RyanMKrol <ryankrol.m@gmail.com>
2020-12-14 17:59:34 +0000

    Refactor main daemon
    * This will now only run once a week, fetching all historic
    price data for all stocks that we support. This will ensure
    that if a new stock enters the index, we'll fetch all of the
    data for it in the next cycle
    
    * This is very ineffecient at the moment because we're fetching
    records for millions of items that we already have. This also
    takes like 5 days to run, so it's far from an ideal solution.
    
    * This should be fine for the short term though

--- src/daemon/index.js
+++ src/daemon/index.js
@@ -1,14 +1,12 @@
+/* eslint-disable no-async-promise-executor */
+
 import schedule from 'node-schedule';
-import semaphore from 'semaphore';
 
 import { MAIL_CLIENT, info } from '../modules/constants';
 import updateTickersData from './tickers';
 import updateFundamentalsData from './fundamentals';
 import updateHeatmapsData from './heatmaps';
-import { updateRecentPricesData } from './prices';
-
-// will be used to lock the main function to one call at a time
-const sem = semaphore(1);
+import { updateHistoricPricesData } from './prices';
 
 /**
  * A method to setup a schedule for the API data
@@ -16,12 +14,12 @@ const sem = semaphore(1);
 async function updateServiceData() {
   // always run this on the first call to ensure that the store is populated
   // on service start
-  main().catch((e) => {
-    MAIL_CLIENT.sendMail('Failed to update the StockAPI data!', e.toString());
+  updateLocalData().catch((e) => {
+    MAIL_CLIENT.sendMail('Failed to update local StockAPI data!', e.toString());
   });
 
-  // update is scheduled to run every three days at midnight
-  schedule.scheduleJob('0 0 0 * * *', async () => {
+  // update is scheduled to run every seven days at midnight
+  schedule.scheduleJob('0 0 0 */7 * *', async () => {
     main().catch((e) => {
       MAIL_CLIENT.sendMail('Failed to update the StockAPI data!', e.toString());
     });
@@ -34,32 +32,49 @@ async function updateServiceData() {
  * @returns {Promise.<void>} A promise to handle errors on
  */
 async function main() {
-  return new Promise((resolve, reject) => {
-    sem.take(async () => {
-      try {
-        await MAIL_CLIENT.sendMail('Starting to update the StockAPI Data!', '');
+  return new Promise(async (resolve, reject) => {
+    try {
+      await MAIL_CLIENT.sendMail('Starting to update the long-term StockAPI Data!', '');
 
-        // daemon's that update the local API data take priority
-        info('Starting tickers data update');
-        await updateTickersData();
-        info('Starting fundamentals data update');
-        await updateFundamentalsData();
-        info('Starting heatmap data update');
-        await updateHeatmapsData();
+      info('Starting price data update');
+      await updateHistoricPricesData();
 
-        // daemon's that don't directly influence the API response can start later
-        info('Starting price data update');
-        await updateRecentPricesData(MAIL_CLIENT);
+      info('Starting local data update');
+      await updateLocalData();
 
-        await MAIL_CLIENT.sendMail('Finished updating the StockAPI data!', '');
+      await MAIL_CLIENT.sendMail('Finished updating the long-term StockAPI data!', '');
 
-        resolve();
-      } catch (e) {
-        reject(e);
-      } finally {
-        sem.leave();
-      }
-    });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+/**
+ * Updates the local API's data using either current long term storage, or "lightweight"
+ fetches
+ *
+ * @returns {Promise.<void>} A promise to handle errors on
+ */
+async function updateLocalData() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await MAIL_CLIENT.sendMail('Starting to update the local StockAPI Data!', '');
+
+      info('Starting tickers data update');
+      await updateTickersData();
+      info('Starting fundamentals data update');
+      await updateFundamentalsData();
+      info('Starting heatmap data update');
+      await updateHeatmapsData();
+
+      await MAIL_CLIENT.sendMail('Finished updating the local StockAPI Data!', '');
+
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
   });
 }
 
